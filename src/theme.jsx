af6d2e92b1e13a4ca931dc82143453f474c71910
Matheus Massari <massari.matheus@gmail.com>
2023-03-31 19:38:11 -0300

    Theme scrollbar

--- src/theme.jsx
+++ src/theme.jsx
@@ -5,6 +5,17 @@ const fonts = { mono: `'Menlo', monospace` };
 const theme = extendTheme({
     styles: {
         global: {
+            "::-webkit-scrollbar": {
+                width: "0.5em",
+                height: "0.5em",
+            },
+            "::-webkit-scrollbar-thumb": {
+                background: "#f2dcbc",
+                borderRadius: "1em",
+            },
+            "::-webkit-scrollbar-track": {
+                background: "transparent",
+            },
             html: {
                 bgColor: "#E8E6E3",
             },
